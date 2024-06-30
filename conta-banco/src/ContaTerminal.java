66898e7c28fcb9ba95d079867e5d33f634991509
alex <alexvieira.360@hotmail.com>
2024-06-30 19:49:24 -0300

    commit adicionando tramento de excecao ao projeto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ContaTerminal {
@@ -8,35 +9,40 @@ public class ContaTerminal {
         String nomeCliente;
         double saldo;
 
-        // TODO: Conhecer e importar a classe Scanner.
-        Scanner scanner = new Scanner(System.in);
-
-        // TODO: Exibir as mensagens para nosso usuário.
-        // TODO: Obter pelo o scanner os valores digitado no terminal.
         try {
+
+            // TODO: Obter pelo o scanner os valores digitado no terminal.
+            Scanner scanner = new Scanner(System.in);
+
+            // TODO: Exibir as mensagens para nosso usuário.
             System.out.println("Por favor, digite o número da Agência !");
             agencia = scanner.next();
 
             System.out.println("Por favor, digite o número da Conta !");
             numero = scanner.nextInt();
 
-            System.out.println("Por favor, digite o Nome do cliente !");
             scanner.nextLine();
+
+            System.out.println("Por favor, digite o Nome do cliente !");
             nomeCliente = scanner.nextLine();
 
             System.out.println("Por favor, digite o Valor !");
             String saldoInput = scanner.next();
             saldo = Double.parseDouble(saldoInput.replace(",", "."));
-            
+
             // TODO: Exibir a mensagem da conta criada.
             System.out.println("Olá " + nomeCliente +
                     ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
                     " conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
 
-        } catch (Exception e) {
-            System.out.println("Ocorreu um erro. Verifique se os valores inseridos estão corretos.");
-        } finally {
             scanner.close();
+
+        } catch (InputMismatchException expVerificaCampoNumeroESaldo) {
+            System.out.println("Os campos numero e saldo prescisam se numericos.");
+        } catch (NumberFormatException expVerificarCampoSaldo) {
+            System.out.println("O campo saldo prescisa se numerico.");
+        } catch (Exception expVerificarCampos) {
+            System.out.println("Ocorreu um erro. Verifique se os valores inseridos estão corretos.");
         }
 
     }
